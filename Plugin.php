e2506099467e11317922aa02026335c913fe5997
daslicht <daslicht@ansolas.de>
2016-09-15 13:35:28 +0200

    Removed Debug Messages, added readme

--- Plugin.php
+++ Plugin.php
@@ -17,12 +17,13 @@ class Plugin extends PluginBase
 
             'functions' => [
                 'downloadAll' => function ( $downloads ) { 
+                    // Bundel File Name 
                     $bundle_file_name = 'BrebLinerSevicesDocuments.zip';
                     $root = $_SERVER['DOCUMENT_ROOT'];
                     $public_path = 'storage/temp/public/';
                     $server_zip_path =  temp_path() . '/public/'. $bundle_file_name;
 
-                   // check if file exists and delte it
+                    // check if file exists and delte it
                     if ( file_exists($server_zip_path) ) {
                         unlink( $server_zip_path );
                     }
@@ -33,16 +34,9 @@ class Plugin extends PluginBase
             
                     // add Files to the above created ZIP
                     foreach ( $downloads as $download ) {
-                        // $extension = pathinfo(parse_url($download['file'])['path'], PATHINFO_EXTENSION);
                         $filename = basename( $download['file']);
-                        
                         $server_file_path = $root . '/storage/app/media'. $download['file'];
-                        // echo $server_file_path;
-                       
                         $zip->addFile( $server_file_path, $filename );
-                        // echo $server_zip_path;
-                    
-                        // echo "E:". file_exists($server_file_path ) . "<br>";
                     }  
 
                     $zip->close();    
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DownloadBundle
+Usage: 
+```
+<a href="{{ downloadAll(this.theme.liner_services_downloads) }}" download>Download all documents</a>
+```
